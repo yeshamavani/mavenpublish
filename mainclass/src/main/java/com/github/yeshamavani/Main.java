3a90e904bb3f8b4fdea6f882d13316a86ad3d5a9
yesha.mavani <yesha.mavani@sourcefuse.com>
2023-03-25 13:42:02 +0530

    init

---
+++ mainclass/src/main/java/com/github/yeshamavani/Main.java
@@ -0,0 +1,9 @@
+package com.github.yeshamavani;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        String s = SharedLibrary.getAppName();
+        System.out.println(s);
+    }
+}
